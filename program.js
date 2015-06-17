aa571c7128dd50085fa9ff782c034489a7ac0c1d
Shyam Sundar Bukka <shyam.someonespecial@gmail.com>
2015-06-16 22:41:44 -0700

    refactor http client

--- program.js
+++ program.js
@@ -2,7 +2,6 @@ var http = require('http');
 
 http.get(process.argv[2], function(response){
     response.setEncoding('utf-8');
-    response.on('data', function(data){
-        console.log(data);
-    })
+    response.on('data', console.log);
+    response.on('error', console.error);
 })
