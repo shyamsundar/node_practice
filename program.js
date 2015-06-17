5a83ece499240ced20e091c90bc0b795746fafef
Shyam Sundar Bukka <shyam.someonespecial@gmail.com>
2015-06-16 19:51:26 -0700

    initial commit

---
+++ program.js
@@ -0,0 +1,27 @@
+var fs = require("fs");
+var path = require('path');
+
+var filetoRead = process.argv[2];
+var fileFilter = process.argv[3];
+
+/*fs.readFile(filetoRead, function(err,data){
+   console.log(data.toString().split("\n").length - 1); 
+});*/
+
+/*
+var contents = fs.readFileSync(filetoRead);
+console.log(contents.toString().split("\n").length - 1);
+*/
+
+/*fs.readdir(filetoRead, function (err, files) {
+    files.forEach(function(elem){
+        if (elem.substr(-(fileFilter.length + 1)) == "." + fileFilter)
+            console.log(elem);
+    });
+})*/
+
+fs.readdir(filetoRead, function(err, files){
+    files.forEach(function(elem){
+        if(path.extname(elem) == "." + fileFilter) console.log(elem);
+    })
+})
