b0ce6f47317e9af69738de99419f64c1a9a5981f
Shyam Sundar Bukka <shyam.someonespecial@gmail.com>
2015-06-22 20:43:39 -0700

    excercise 8 type 1

--- program.js
+++ program.js
@@ -2,6 +2,13 @@ var http = require('http');
 
 http.get(process.argv[2], function(response){
     response.setEncoding('utf-8');
-    response.on('data', console.log);
+    respData = '';
+    response.on('data', function(data){
+        respData += data;
+    });
+    response.on('end', function(){
+        console.log(respData.length);
+        console.log(respData);
+    })
     response.on('error', console.error);
 })
